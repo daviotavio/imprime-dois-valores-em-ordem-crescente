1fab731f632035188418f6b5f81045183a6378ae
Davi Otavio <85647007+daviotavio@users.noreply.github.com>
2021-09-06 12:00:48 -0300

    Add files via upload

---
+++ ordem crescente/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ordem_crescente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint valor1,valor2;
+
+            Console.Write("Digite um valor inteiro positivo: ");
+            valor1 = uint.Parse(Console.ReadLine());
+
+            Console.Write("Digite outro valor inteiro positivo: ");
+            valor2 = uint.Parse(Console.ReadLine());
+
+            if (valor1 > valor2)
+            {
+                Console.WriteLine(valor2);
+                Console.WriteLine(valor1);
+            }
+            else
+            {
+                Console.WriteLine(valor1);
+                Console.WriteLine(valor2);
+            }
+        }
+    }
+}
